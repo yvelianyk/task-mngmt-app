d3021ce19c4fe3d8de5242bbe370ab4ce2a0222f
yvelianyk <yvelianyk@prismhr.com>
2020-01-15 10:37:28 +0200

    init commit

---
+++ src/main/java/com/yvelianyk/taskmanagementapplesson/LsApplication.java
@@ -0,0 +1,13 @@
+package com.yvelianyk.taskmanagementapplesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yvelianyk/taskmanagementapplesson/TaskManagementAppLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yvelianyk.taskmanagementapplesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementAppLessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
