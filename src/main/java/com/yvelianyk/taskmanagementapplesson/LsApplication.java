d532058815ab49ffa99ea0fac366b61ddbfaae05
yvelianyk <yvelianyk@prismhr.com>
2020-01-28 10:20:47 +0200

    feat: add logging into the project

--- src/main/java/com/yvelianyk/taskmanagementapplesson/LsApplication.java
+++ src/main/java/com/yvelianyk/taskmanagementapplesson/LsApplication.java
@@ -9,12 +9,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
 import java.time.LocalDate;
+import java.util.Optional;
 
 @SpringBootApplication()
 public class LsApplication {
 
-//	@Autowired
-//	@Qualifier("projectServiceImpl") IProjectService projectService;
+	@Autowired
+	@Qualifier("projectServiceImpl") IProjectService projectService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LsApplication.class, args);
@@ -22,7 +23,9 @@ public class LsApplication {
 
 	@PostConstruct
 	public void postConstruct() {
-		// projectService.save(new Project(1L, "My first Project", LocalDate.now()));
+		projectService.save(new Project(1L, "My first Project", LocalDate.now()));
+		Optional<Project> optionalProject = projectService.findById(1L);
+		optionalProject.ifPresent(System.out::println);
 	}
 
 }
--- src/main/java/com/yvelianyk/taskmanagementapplesson/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/yvelianyk/taskmanagementapplesson/service/impl/ProjectServiceImpl.java
@@ -3,7 +3,8 @@ package com.yvelianyk.taskmanagementapplesson.service.impl;
 import com.yvelianyk.taskmanagementapplesson.persistence.model.Project;
 import com.yvelianyk.taskmanagementapplesson.persistence.repository.IProjectRepository;
 import com.yvelianyk.taskmanagementapplesson.service.IProjectService;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,6 +12,7 @@ import java.util.Optional;
 @Service
 public class ProjectServiceImpl implements IProjectService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ProjectServiceImpl.class);
     private IProjectRepository projectRepository;
 
     public ProjectServiceImpl(IProjectRepository projectRepository) {
@@ -19,11 +21,13 @@ public class ProjectServiceImpl implements IProjectService {
 
     @Override
     public Optional<Project> findById(Long id) {
+        LOG.debug("Project Service >> Finding Project By Id {}", id);
         return projectRepository.findById(id);
     }
 
     @Override
     public Project save(Project project) {
+        LOG.debug("Project Service >> Saving Project {}", project);
         return projectRepository.save(project);
     }
 }
